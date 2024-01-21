4b3d7dc5c882b2a6ef33b840dd05af8dd8f46783
Arsymz <136714110+Arsymz@users.noreply.github.com>
2024-01-22 06:35:40 +0800

    Delete app directory

--- app/src/main/java/com/example/music/navigation/Screen.kt
+++
@@ -1,6 +0,0 @@
-package com.example.music.navigation
-
-sealed class Screen(val route: String){
-/*    object LogoScreen : Screen("logo_screen")
-    object CategoryScreen : Screen("category_screen")*/
-}
